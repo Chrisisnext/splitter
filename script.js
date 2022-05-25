8f7a6f52068752dc704441b477048ece9fdc4712
Nick Axworthy <nickax@gmail.com>
2022-05-25 15:10:36 +0100

    Fixed broken code

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 "use strict"
 
+
 function split4(e){
 
     if(e.shiftKey){
         //shift is pressed - lets all the children of my parent (that includes me!)
         this.parentNode.addEventListener("click",split4)
         this.parentNode.innerHTML=''  //or delete the children a nicer way
-        e.stopPropagation() //stop the event from firing again on all the ancestor divs (now we added the listener back !)            
     }
     else{
         //Split a div into 4 by adding 4 quarters
 
-        this.removeEventListener("click",split4)
+       // this.removeEventListener("click",split4)
         addQuarter(this)
         addQuarter(this)
         addQuarter(this)
         addQuarter(this)
     }
+
+    // Event 'capturing and bubbling' 
+    e.stopPropagation() //stop the event from firing again on all the ancestor divs
 }
 
 
@@ -31,9 +34,41 @@ function addQuarter(intoDiv){
 
     q.addEventListener("click",split4)
 
+
+
+
     intoDiv.appendChild(q)
 
 }
 
 document.body.addEventListener("click",split4)
+
+
+
+function moved(e){
+    
+    //console.log(e.clientX + " " + e.clientY)
+    //distance between e.clientX,e. clienty - and treasureX, Treasure
+
+    let box = document.getElementById("box")
+    box.style.left = e.clientX+"px"
+    box.style.top = e.clientY+"px"
+
+}
+
+//document.body.addEventListener("mousemove",moved)
+
+//alert("hello")
+
+/*
+function key(e){
+    alert(e.key)
+    
+}
+document.body.addEventListener("keydown",key)
+
+*/
+
+
+
 
